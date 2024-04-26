91184ebb4d05f62e8eaaa8bff26d61cd359dbfea
Patricio Pilco <ppilco@galavail.com>
2024-04-26 09:43:37 -0500

    Añadir soporte para NATS en la aplicación

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -19,36 +19,29 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  //@Post()
   @MessagePattern({ cmd: 'create-product' })
   create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
-  //@Get()
   @MessagePattern({ cmd: 'find-all-products' })
   findAll(@Payload() paginationDto: PaginationDto) {
     console.log('paginationDto', paginationDto);
     return this.productsService.findAll(paginationDto);
   }
 
-  //@Get(':id')
   @MessagePattern({ cmd: 'find-one-product' })
   findOne(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(id);
   }
 
-  //@Patch(':id')
   @MessagePattern({ cmd: 'update-product' })
   update(
-    //@Param('id', ParseIntPipe) id: number,
-    //@Body() updateProductDto: UpdateProductDto,
     @Payload() updateProductDto: UpdateProductDto
   ) {
     return this.productsService.update(updateProductDto.id, updateProductDto);
   }
 
-  //@Delete(':id')
   @MessagePattern({ cmd: 'delete-product' })
   remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
